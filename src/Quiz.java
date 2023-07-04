a3776ac112495ef3afd7cc9fa948e4945bf0e5a1
SchutzenderZeus <maximilian.jatzlau@schiller-og.de>
2023-07-04 09:13:57 +0200

    aa

---
+++ src/Quiz.java
@@ -0,0 +1,43 @@
+public class Quiz {//Text adventure mit Quiz
+
+
+    static int points = 3;
+
+    public static void main(String[] args) {
+        waitMilliseconds(1000);
+        System.out.print("Hello");
+        makePoints(500, " ~???");
+        waitMilliseconds(1000);
+        System.out.print("\nYou have a choice to make");
+        makePoints(500, " ~???");
+        System.out.print("\n\nWould you like to"
+                        + "\n a) Leave your House"
+                        + "\n b) Go upstairs"
+                        + "\n c) Wait");
+
+        if
+
+    }
+
+
+
+    public static void waitMilliseconds(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void makePoints(int millis1, String str) {
+        for (int i = 0; i < points; i++){
+            waitMilliseconds(millis1);
+            System.out.print(".");
+        }
+        waitMilliseconds(millis1);
+        System.out.print(str);
+    }
+
+
+}
+
